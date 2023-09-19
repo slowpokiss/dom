3beaec206ba969328d727f31d9586ee84323d7bd
Michel <hltvexe@mail.ru>
2023-09-19 21:18:24 +0300

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+import goblinPNG from "../img/goblin.png";
+const field = document.querySelectorAll(".field");
+
+function intervalFunc() {
+  let currNumb = 0;
+  const goblin = `<img src='${goblinPNG}' class="goblin" alt="goblin"></img>`;
+  setInterval(() => {
+    let randNumb = Math.floor(Math.random() * field.length);
+    field[currNumb].innerHTML = "";
+    if (currNumb !== randNumb) {
+      currNumb = randNumb;
+      field[currNumb].insertAdjacentHTML("beforeend", goblin);
+    } else {
+      while (currNumb === randNumb) {
+        randNumb = Math.floor(Math.random() * field.length);
+      }
+      currNumb = randNumb;
+      field[currNumb].insertAdjacentHTML("beforeend", goblin);
+    }
+  }, 1000);
+}
+
+intervalFunc();
